73bd751f6cc6bfab09ba05ef50886ef72d738a43
Jordan Jeffries <90280518+jmjeffries@users.noreply.github.com>
2024-10-15 17:54:44 -0400

    Add files via upload

---
+++ transparency/forms.py
@@ -0,0 +1,9 @@
+from django.db import models  
+from django.forms import fields  
+from django import forms
+from .models import UploadImage
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        models = UploadImage
+        fields = '__all__'
